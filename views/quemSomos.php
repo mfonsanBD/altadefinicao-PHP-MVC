4272a699fe38befd84c1c8e4f909ef45452e63f9
Mike Santos <iddeiasimplesmike@gmail.com>
2020-07-04 11:00:09 -0300

    Inclusão de card para missão, visão e valores e os cards da equipe

--- views/quemSomos.php
+++ views/quemSomos.php
@@ -1,30 +1,103 @@
 <nav aria-label="breadcrumb">
-    <ol class="breadcrumb justify-content-center ondeestou">
+    <ol class="breadcrumb justify-content-center ondeestou bg-dark">
         <li class="breadcrumb-item"><a href="inicio">Página Inicial</a></li>
-        <li class="breadcrumb-item active" aria-current="page"><?=$this->titulo?></li>
+        <li class="breadcrumb-item text-white active" aria-current="page"><?=$this->titulo?></li>
     </ol>
 </nav>
 <section id="quem-somos" class="quem-somos">
     <div class="container">
-        <h1 class="text-uppercase text-center titulo"><?=$this->titulo?></h1>
         <div class="row mt-5">
             <div class="col-lg-6">
-                <div class="quemSomosImg">
-                    <img src="https://fotografiamais.com.br/wp-content/uploads/2019/01/cmyk-ou-rgb-o-que-significa.jpg" alt="Quem Somos" width="100%">
-                </div>
+                <div class="quemSomosImg"></div>
                 <div class="caixaImg"></div>
             </div>
             <div class="col-lg-6">
                 <div class="quemSomosText">
-                    <h3>Sobre Nós</h3>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. In tempora molestias ad adipisci optio laudantium rem! Eligendi, sed blanditiis eveniet reprehenderit, assumenda dolores quis delectus illo nihil accusantium adipisci alias.</p>
+                    <p class="text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In tempora molestias ad adipisci optio laudantium rem! Eligendi, sed blanditiis eveniet reprehenderit, assumenda dolores quis delectus illo nihil accusantium adipisci alias.</p>
                 </div>
             </div>
         </div>
     </div>
 </section>
+<section id="quem-somos" class="quem-somos mvv">
+    <div class="container">
+        <div class="row">
+            <div class="col bg-white rounded-lg shadow p-3 ml-1 mr-1 text-center m-0">
+                <i class="fas fa-mountain fa-3x text-warning"></i>
+                <h6 class="mt-3 text-uppercase">Missão</h6>
+                <p class="text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In tempora molestias ad adipisci optio laudantium rem!</p>
+            </div>
+            <div class="col bg-white rounded-lg shadow p-3 ml-1 mr-1 text-center m-0">
+                <i class="fas fa-bullseye fa-3x text-warning"></i>
+                <h6 class="mt-3 text-uppercase">Visão</h6>
+                <p class="text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In tempora molestias ad adipisci optio laudantium rem!</p>
+            </div>
+            <div class="col bg-white rounded-lg shadow p-3 ml-1 mr-1 text-center m-0">
+                <i class="far fa-handshake fa-3x text-warning"></i>
+                <h6 class="mt-3 text-uppercase">Valores</h6>
+                <p class="text-muted">Lorem ipsum, dolor sit amet consectetur adipisicing elit. In tempora molestias ad adipisci optio laudantium rem!</p>
+            </div>
+        </div>    
+    </div>
+</section>
 <section id="quem-somos" class="quem-somos">
     <div class="container">
-        <h1 class="text-uppercase text-center titulo">Nossa Equipe</h1>      
+        <h1 class="text-uppercase text-center titulo">Nossa Equipe</h1>
+        <div class="row row-cols-1 row-cols-lg-3 mt-5">
+            <div class="col mb-4">
+                <div class="card">
+                <img src="assets/img/equipe/valdeir.webp" class="card-img-top" alt="Valdeir Foli">
+                <div class="card-body">
+                    <h5 class="card-title">Valdeir<br>Foli</h5>
+                    <p class="card-text">CEO.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                <img src="assets/img/equipe/kamily.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Kamily<br>Foli</h5>
+                    <p class="card-text">Administração.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                <img src="assets/img/equipe/daniel.webp" class="card-img-top" alt="Daniel Valdez">
+                <div class="card-body">
+                    <h5 class="card-title">Daniel<br>Valdez</h5>
+                    <p class="card-text">Gerência.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                <img src="assets/img/equipe/junior.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Júnior<br>Valdez</h5>
+                    <p class="card-text">Recorte/Router.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                <img src="https://varejointeligente.cdlbh.com.br/wp-content/uploads/woocommerce-placeholder-1170x1170.png" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Vinícius<br>Maracanã</h5>
+                    <p class="card-text">Acabamento.</p>
+                </div>
+                </div>
+            </div>
+            <div class="col mb-4">
+                <div class="card">
+                <img src="assets/img/equipe/mike.webp" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">Mike<br>Santos</h5>
+                    <p class="card-text">Arte Final.</p>
+                </div>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
