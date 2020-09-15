9470a3d5f5337cf67ead67bb7b3c006299dff682
Mike Santos <iddeiasimplesmike@gmail.com>
2020-09-15 17:41:32 -0300

    Galeria de produtos

--- views/pes/letreiro-em-acm.php
+++ views/pes/letreiro-em-acm.php
@@ -6,33 +6,33 @@ require('breadcrumb.php');
     <div class="tz-gallery">
         <div class="row">
             <div class="col-sm-12 col-md-4">
-                <a class="lightbox" href="assets/img/servicos-produtos/acm/bridge.jpg">
-                    <img src="assets/img/servicos-produtos/acm/bridge.jpg" alt="Bridge">
+                <a class="lightbox" href="assets/img/servicos-produtos/acm/acm-01.jpg">
+                    <img src="assets/img/servicos-produtos/acm/acm-01.jpg" alt="Bridge">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="assets/img/servicos-produtos/acm/park.jpg">
-                    <img src="assets/img/servicos-produtos/acm/park.jpg" alt="Park">
+                <a class="lightbox" href="assets/img/servicos-produtos/acm/acm-02.jpg">
+                    <img src="assets/img/servicos-produtos/acm/acm-02.jpg" alt="Park">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="assets/img/servicos-produtos/acm/tunnel.jpg">
-                    <img src="assets/img/servicos-produtos/acm/tunnel.jpg" alt="Tunnel">
+                <a class="lightbox" href="assets/img/servicos-produtos/acm/acm-03.jpg">
+                    <img src="assets/img/servicos-produtos/acm/acm-03.jpg" alt="Tunnel">
                 </a>
             </div>
-            <div class="col-sm-12 col-md-8">
-                <a class="lightbox" href="assets/img/servicos-produtos/acm/traffic.jpg">
-                    <img src="assets/img/servicos-produtos/acm/traffic.jpg" alt="Traffic">
+            <div class="col-sm-12 col-md-4">
+                <a class="lightbox" href="assets/img/servicos-produtos/acm/acm-04.jpg">
+                    <img src="assets/img/servicos-produtos/acm/acm-04.jpg" alt="Traffic">
                 </a>
             </div>
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="assets/img/servicos-produtos/acm/coast.jpg">
-                    <img src="assets/img/servicos-produtos/acm/coast.jpg" alt="Coast">
+                <a class="lightbox" href="assets/img/servicos-produtos/acm/acm-05.jpg">
+                    <img src="assets/img/servicos-produtos/acm/acm-05.jpg" alt="Coast">
                 </a>
             </div> 
             <div class="col-sm-6 col-md-4">
-                <a class="lightbox" href="assets/img/servicos-produtos/acm/rails.jpg">
-                    <img src="assets/img/servicos-produtos/acm/rails.jpg" alt="Rails">
+                <a class="lightbox" href="assets/img/servicos-produtos/acm/acm-06.jpg">
+                    <img src="assets/img/servicos-produtos/acm/acm-06.jpg" alt="Rails">
                 </a>
             </div>
         </div>
